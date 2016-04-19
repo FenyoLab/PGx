4c8c5463cb104946ad58430db97b82e635a8741e
Manor Askenazi <mass.informatics@gmail.com>
2016-04-19 16:42:14 -0400

    Eliminate requirement for oneliner proteins
    The original implementation of pgx_index.py assumed that the FASTA file
    provided by the user had exactly one line per protein. Since many sites
    do not preprocess FASTA files such that each protein occupies a single
    line, it was necessary to loosen the requirements to allow all FASTA
    files as input.

--- pgx_index.py
+++ pgx_index.py
@@ -5,30 +5,45 @@ start = time.time()
 
 import cPickle
 
-
 def index(proteome_path, q=4):
-    #
-    # Proteins in the target_file must all occupy one line exactly!
-    #
     proteins = {}
     peptides = {}
+    code_container = [0] # This only exists because closures are not possible in Python2.7 (no "nonlocal" keyword)
 
+    def process_entry(acc,seq):
+        #
+        # This closure exists only to deal with the fact that there is no mandated endfile delimiter in FASTA files
+        # (and I refused to write the same code twice in the line processing for-loop). If anybody has a more elegant
+        # way to do this, I'm all ears! (deposit in pull-request/suggestion-box)
+        #
+        if seq == "" or acc == "": # This is both useful as a sanity check and eliminates if statements for the external for loop.
+            return
+        #
+        # Python 2.7 closures are broken: no "nonlocal" keyword
+        # see: https://www.python.org/dev/peps/pep-3104/
+        code = code_container[0] # 
+        code += 1                # 
+        code_container[0] = code # 
+        proteins[code] = (acc, seq)
+        for i in range(len(seq)-q+1):
+            pep = seq[i:i+q].replace('L', 'I')
+            if not pep in peptides:
+                peptides[pep] = set()
+            peptides[pep].add(code)
+    
     acc = ""
-    code = 0
+    seq = ""
     f = open(proteome_path + "proteome.fasta")
     for l in f:
         if l.startswith(">"):
+            process_entry(acc,seq)
+            seq = ""
             acc = l.strip()[1:]
         else:
-            #The entire protein must be on a single line!!!
-            code += 1
-            seq = l.strip()
-            proteins[code] = (acc, seq)
-            for i in range(len(seq)-q+1):
-                pep = seq[i:i+q].replace('L', 'I')
-                if not pep in peptides:
-                    peptides[pep] = set()
-                peptides[pep].add(code)
+            nu_seq = l.strip()
+            seq += nu_seq
+    process_entry(acc,seq)
+
     db_name = proteome_path + "proteome.pickle"
     f = open(db_name, 'wb')
     cPickle.dump(q, f)
